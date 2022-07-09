008b3a4fa7b7ed617f0e9563d69467701d01fab3
mdo <mdo@ringblaze.com>
2022-07-09 20:54:01 +0700

    chore: cleanup

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -26,7 +26,7 @@ function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>ShareTube</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
